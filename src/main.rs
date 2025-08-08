696aa81f2e35589ca45b389f0cdf8d85cbdf832b
Shay Doherty <shaydoherty15@yahoo.co.uk>
2025-08-08 20:24:12 +0100

    save multiple passwords

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,148 @@
 use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::fs;
-use std::io::{self, Write, Read};
+use std::io::{self, Write};
 use std::path::Path;
 
-// create a struct to hold the configuration data
 #[derive(Serialize, Deserialize)]
-struct InnerConfig{
+struct Config {
+    app_name: String,
     username: String,
     password: String,
 }
-#[derive(Serialize, Deserialize)]
-struct OuterConfig {
-    app_name: String,
-    config: InnerConfig,
-}
 
+ 
 fn main() {
-    
+    loop {
+        let user_choice = prompt_user();
+        match user_choice {
+            1 => {
+                // Check if the file already exists
+                if Path::new("password.json").exists() {
+                    
+                    
+                    // prompt the user for configuration details
+                    let mut app_name = String::new();
+                    println!("Enter the application name:");
+                    io::stdin().read_line(&mut app_name).expect("Failed to read application name");
 
-    // Check if the file exists
-   loop{ 
-    let mut user_choice = prompt_user();
-    match user_choice {
-        1 => {
-        // Prompt the user for configuration details
-        let mut app_name = String::new();
-        println!("Enter the application name:");
-        io::stdin().read_line(&mut app_name).expect("Failed to read application name");
-        let mut username = String::new();
-        println!("Enter the username:");
-        io::stdin().read_line(&mut username).expect("Failed to read username");
-        let mut password = String::new();
-        println!("Enter the password:");
-        io::stdin().read_line(&mut password).expect("Failed to read password");
-        
-        // Create a Config instance
-            let mut inner_config = InnerConfig {
-                username: username.trim().to_string(),
-                password: password.trim().to_string(),
-            };
-            let outer_config = OuterConfig {
-                app_name: app_name.trim().to_string(),
-                config: inner_config,
-            };
-            // Serialize the config to a JSON file
-            let path = Path::new("password.json");
-            if path.exists() {
-                println!("File already exists. Overwriting...");
-            };
+                    let mut username = String::new();
+                    println!("Enter the username:");
+                    io::stdin().read_line(&mut username).expect("Failed to read username");
 
-            // Write the config to a JSON file
-            let json = serde_json::to_string_pretty(&outer_config)
-                .expect("Failed to serialize config");
+                    let mut password = String::new();
+                    println!("Enter the password:");
+                    io::stdin().read_line(&mut password).expect("Failed to read password");
 
-            // Create or open the file
-            let mut file = File::create("password.json").expect("Failed to create file");
-            file.write_all(json.as_bytes()).expect("Failed to write to file");
-        }
-        2 => {
-            println!("enter the app you want to view the password for:");
-            // Read the website name from user input
-            let mut app = String::new();
-            io::stdin().read_line(&mut app).expect("Failed to read app name");
-            let app = app.trim();
 
-            let config_contents = fs::read_to_string("password.json").expect("Failed to read password.json");
+                    // Create a Config instance
+                    let config = Config {
+                    app_name: app_name.trim().to_string(),
+                    username: username.trim().to_string(),
+                    password: password.trim().to_string(),
+                    };
 
-            // Parse the whole JSON once
-            let config: OuterConfig = serde_json::from_str(&config_contents).expect("Failed to parse JSON");
+                    // read existing configs from the file
+                    let config_contents = fs::read_to_string("password.json").expect("Failed to read password.json");
 
-            // Compare app_name with user input
-            if config.app_name.eq_ignore_ascii_case(app) {
-                println!("App Name: {}", config.app_name);
-                println!("Username: {}", config.config.username);
-                println!("Password: {}", config.config.password);
-            } else {
-                println!("No password found for the specified app.");
+                    let mut configs: Vec<Config> = serde_json::from_str(&config_contents).expect("Failed to parse JSON");
+
+                    // Add the new config to the existing configs
+                    configs.push(config);
+
+                    let json = serde_json::to_string_pretty(&configs).expect("Failed to serialize config");
+
+                    // Write the config to a JSON file
+                    let mut file = File::create("password.json").expect("Failed to create file");
+                    file.write_all(json.as_bytes()).expect("Failed to write to file");
+
+
+                }
+                else{
+                // create vector to store configs    
+                let mut configs: Vec<Config> = Vec::new();
+
+
+                // Prompt the user for configuration details
+                let mut app_name = String::new();
+                println!("Enter the application name:");
+                io::stdin().read_line(&mut app_name).expect("Failed to read application name");
+
+                let mut username = String::new();
+                println!("Enter the username:");
+                io::stdin().read_line(&mut username).expect("Failed to read username");
+
+                let mut password = String::new();
+                println!("Enter the password:");
+                io::stdin().read_line(&mut password).expect("Failed to read password");
+
+                // Create a Config instance
+                let config = Config {
+                    app_name: app_name.trim().to_string(),
+                    username: username.trim().to_string(),
+                    password: password.trim().to_string(),
+                };
+
+                // Add the config to the vector
+                configs.push(config);
+
+
+                // Serialize to JSON
+                let json = serde_json::to_string_pretty(&configs).expect("Failed to serialize config");
+
+                // Write the config to a JSON file
+                let mut file = File::create("password.json").expect("Failed to create file");
+                file.write_all(json.as_bytes()).expect("Failed to write to file");
+
+                println!("Password saved successfully.");
+            }
+            }
+            2 => {
+                println!("Enter the app you want to view the password for:");
+                let mut app = String::new();
+                io::stdin().read_line(&mut app).expect("Failed to read app name");
+                let app = app.trim();
+
+                let config_contents = fs::read_to_string("password.json").expect("Failed to read password.json");
+
+                let configs: Vec<Config> = serde_json::from_str(&config_contents).expect("Failed to parse JSON");
+
+
+                for config in configs {
+                    if config.app_name.eq_ignore_ascii_case(app) {
+                        println!("App Name: {}", config.app_name);
+                        println!("Username: {}", config.username);
+                        println!("Password: {}", config.password);
+                    } else {
+                        println!("No password found for the specified app.");
+                    }
+                }
+            }
+            3 => {
+                println!("Exiting the program.");
+                break;
+            }
+            _ => {
+                println!("Invalid input. Please enter a number between 1 and 3.");
             }
         }
-        3 => {
-            println!("Exiting the program.");
-            break;
-        }
-        _ => {
-            println!("Invalid input. Please enter a number between 1 and 3.");
-        }
-}
-}
+    }
 }
 
 fn prompt_user() -> i32 {
-    println!("Please enter your input:");
-    println!("enter 1 to add a new password");
-    println!("enter 2 view list of passwords");
-    println!("enter 3 to exit the program");
-    let mut user_input = String::new();
-    std::io::stdin().read_line(&mut user_input).expect("Failed to read line");
-    let int_input: i32 = user_input.trim().parse().expect("Please enter a number");
+    println!("\nPlease enter your input:");
+    println!("1. Add a new password");
+    println!("2. View saved password");
+    println!("3. Exit");
 
-    println!("You entered: {}", int_input);
-    return int_input
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+
+    match user_input.trim().parse::<i32>() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid number entered. Defaulting to 0.");
+            0
+        }
+    }
 }
